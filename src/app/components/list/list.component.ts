ec4d2b3e28284d8340a3aa11b956e4aae385903a
Tamara Organ <tamara@organ.dev.br>
2021-05-24 21:59:27 -0400

    Update - List

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { concat, Subscription } from 'rxjs';
 import { PokedexService } from 'src/app/service/pokedex.service';
 
 @Component({
@@ -7,17 +8,49 @@ import { PokedexService } from 'src/app/service/pokedex.service';
   styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-  pokemons: any[] = [];
+  //pokemons: any[] = [];
+  subscriptions: Subscription[] = [];
+  loading = false;
 
   constructor(private pokedexService: PokedexService) {}
 
   ngOnInit(): void {
-    this.getPokedex();
+    //this.getPokedex();
+    this.loaderMore();
   }
 
-  getPokedex() {
-    this.pokedexService.getPokemons().subscribe((data: any) => {
-      this.pokemons = data.results;
-    });
+  // getPokedex() {
+  //   this.pokedexService.getPokemons().subscribe((data: any) => {
+  //     this.pokemons = data.results;
+  //   });
+  // }
+
+  get pokemons(): any {
+    return this.pokedexService.pokemons;
+  }
+
+  set subscription(subscription: Subscription) {
+    this.subscriptions.push(subscription);
+  }
+
+  getType(pokemon: any): string {
+    return this.pokedexService.getType(pokemon);
+  }
+
+  loaderMore(): void {
+    this.loading = true;
+    this.subscription = this.pokedexService.getNext().subscribe(
+      (response: any) => {
+        this.pokedexService.next = response.next;
+        const details = response.results.map((i: any) =>
+          this.pokedexService.get(i.name)
+        );
+        this.subscription = concat(...details).subscribe((response: any) => {
+          this.pokedexService.pokemons.push(response);
+        });
+      },
+      (error) => console.log('Error Occurred:', error),
+      () => (this.loading = false)
+    );
   }
 }
--- src/app/service/pokedex.service.ts
+++ src/app/service/pokedex.service.ts
@@ -1,21 +1,62 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { environment } from '../../environments/environment'
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokedexService {
-  url = environment.apiUrl + 'pokemon/'
-  pokemons: any[] = []
-  next = ''
+  url = environment.apiUrl + 'pokemon/';
+  _pokemons: any[] = [];
+  _next: string = '';
 
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  get pokemons(): any[] {
+    return this._pokemons;
+  }
 
+  get next(): string {
+    return this._next;
+  }
+
+  set next(next: string) {
+    this._next = next;
+  }
+
+  //Lista todos os Pokemons
   getPokemons(): Observable<any[]> {
-    return this.http.get<any[]>(this.url)
+    return this.http.get<any[]>(this.url);
+  }
+
+  //Lista o type do Pokemons (se ele é fogo, agua, ou normal) objeto
+  getType(pokemon: any): string {
+    return pokemon && pokemon.types.length > 0 ? pokemon.types[0].type.name : '';
+  }
+
+  //Lista lista um pokemon
+  get(name: string): Observable<any[]> {
+    const url = `${this.url}${name}`;
+
+    return this.http.get<any[]>(url);
+  }
+
+  //Lista os polemons com um limite de 50 por pagina
+  getNext(): Observable<any[]> {
+    const url = this.next === '' ? `${this.url}?limit=50` : this.next;
+    return this.http.get<any[]>(url);
+  }
+
+  //Lista a evolução do pokemon
+  getEvolution(id: number): Observable<any[]> {
+    const url = `${environment.apiUrl}evolution-chain/${id}`;
+    return this.http.get<any[]>(url);
+  }
+
+  //Lista a especie do pokemon
+  getSpecies(name: string): Observable<any[]> {
+    const url = `${environment.apiUrl}pokemon-species/${name}`;
+    return this.http.get<any[]>(url);
   }
 }
-
